da07264e06efbcec72e3b75ae9541163e6abd56b
Delphine <delphinelcrx@gmail.com>
2023-05-14 15:36:33 +0200

    List of patients on doctors

--- src/app/component/dashboard/doctor/view-doctor/view-doctor.component.ts
+++ src/app/component/dashboard/doctor/view-doctor/view-doctor.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit, ViewChild } from "@angular/core"
+import { MatPaginator } from "@angular/material/paginator"
+import { MatSort } from "@angular/material/sort"
+import { MatTableDataSource } from "@angular/material/table"
 import { ActivatedRoute } from "@angular/router"
+import { Patient } from "src/app/shared/model/patient"
 import { DataService } from "src/app/shared/service/data.service"
+import { AddPatientComponent } from "../../patient/add-patient/add-patient.component"
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog"
+import { MatSnackBar } from "@angular/material/snack-bar"
 
 @Component({
   selector: "app-view-doctor",
@@ -10,13 +17,21 @@ import { DataService } from "src/app/shared/service/data.service"
 export class ViewDoctorComponent implements OnInit {
   id!: any
   doctorObj!: any
+  allPatients: Patient[] = []
 
-  constructor(private route: ActivatedRoute, private dataApi: DataService) {
+  displayedColumns: string[] = ["name", "mobile", "gender", "prescription", "action"]
+  dataSource!: MatTableDataSource<Patient>
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort
+
+  constructor(private route: ActivatedRoute, private dataApi: DataService, private dialog: MatDialog, private _snackBar: MatSnackBar) {
     this.id = route.snapshot.paramMap.get("id")
   }
 
   ngOnInit(): void {
     this.getDoctorById()
+    this.getAllPatientsForDoctor()
   }
 
   getDoctorById() {
@@ -24,5 +39,63 @@ export class ViewDoctorComponent implements OnInit {
       this.doctorObj = res
       console.log(this.doctorObj)
     })
+  }
+
+  getAllPatientsForDoctor() {
+    this.dataApi.getAllPatients().subscribe((res) => {
+      this.allPatients = res.map((e: any) => {
+        const data = e.payload.doc.data()
+        data.patient_id = e.payload.doc.id
+
+        if (data.doctor_id == this.id) {
+          return data
+        }
+      })
+
+      this.allPatients = this.allPatients.filter((item) => item != undefined)
+      this.dataSource = new MatTableDataSource(this.allPatients)
+      this.dataSource.paginator = this.paginator
+      this.dataSource.sort = this.sort
+    })
+  }
+
+  viewPatient(row: any) {
+    window.open("dashboard/patient/" + row.patient_id, "_blank")
+  }
+
+  editPatient(row: any) {
+    if (row.patient_id == null || row.patient_name == null) {
+      return
+    }
+    const dialogConfig = new MatDialogConfig()
+    dialogConfig.disableClose = true
+    dialogConfig.autoFocus = true
+    dialogConfig.data = row
+    dialogConfig.data.title = "Edit patient"
+    dialogConfig.data.buttonName = "Update"
+    dialogConfig.data.admission_date = row.admission_date.toDate()
+
+    console.log(dialogConfig.data)
+
+    const dialogRef = this.dialog.open(AddPatientComponent, dialogConfig)
+
+    dialogRef.afterClosed().subscribe((data) => {
+      if (data) {
+        this.dataApi.updatePatient(data)
+        this.openSnackBar("Patient is updated successfully.", "OK")
+      }
+    })
+  }
+  openSnackBar(arg0: string, arg1: string) {
+    throw new Error("Method not implemented.")
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage()
+    }
   }
 }
